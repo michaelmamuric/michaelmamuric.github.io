ff133928dd8ff9efa583e1a15500570007ab9435
michaelmamuric <michael.mamuric@edu.sait.ca>
2020-11-29 11:53:17 -0700

    About Me Page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Layout from '../components/Layout/Layout';
 import WelcomeBanner from '../components/WelcomeBanner/WelcomeBanner';
 import Grid from '@material-ui/core/Grid';
+import 'fontsource-titillium-web';
 import './index.css';
 
 const Home = () => {
