171f0b13d593d4344e8139118dafee9bda2f1fa8
michaelmamuric <michael.mamuric@edu.sait.ca>
2020-11-29 21:09:30 -0700

    Added Meta Tags

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,20 @@ import WelcomeBanner from '../components/WelcomeBanner/WelcomeBanner';
 import About from '../components/About/About';
 import Projects from '../components/Projects/Projects';
 import Contact from '../components/Contact/Contact';
+import Helmet from 'react-helmet';
 import 'fontsource-titillium-web';
 import './index.css';
 
 const Home = () => {
   return (
     <Layout>
+      <Helmet>
+            <meta charset="utf-8" />
+            <title>Michael Mamuric: Software/Web Developer</title>
+            <meta name="keywords" content="Software Developer, Web Developer, Portfolio, Michael Mamuric, Mamuric, Michael Thomas Mamuric" />
+            <meta name="description" content="Personal Portfolio of Michael Mamuric" />
+            <meta name="author" content="Michael Mamuric" />
+      </Helmet>
       <WelcomeBanner />
       <About />
       <Projects />
