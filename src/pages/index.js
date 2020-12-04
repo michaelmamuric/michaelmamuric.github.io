929f57fd888032acdbfb5189eeaa4a8830137aab
michaelmamuric <michael.mamuric@edu.sait.ca>
2020-12-04 14:02:56 -0700

    Re-add google site verification meta tag

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ const Home = () => {
             <title>Michael Mamuric: Software/Web Developer</title>
             <meta name="keywords" content="Software Developer, Web Developer, Portfolio, Michael Mamuric, Mamuric, Michael Thomas Mamuric" />
             <meta name="description" content="Personal Portfolio of Michael Mamuric" />
+            <meta name="google-site-verification" content="cE75Ydj0nQ3VYRAca12N2Q1U_nwDrbfPQJbA6m1TeKA" />
             <meta name="author" content="Michael Mamuric" />
       </Helmet>
       <WelcomeBanner />
